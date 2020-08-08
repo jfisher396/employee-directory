4227caa7ecd5901ced8e107846438d1b1c698ce5
James Fisher <jfisher396@hotmail.com>
2020-08-08 11:24:16 -0700

    linked table data to api

--- src/components/EmployeeDir.js
+++ src/components/EmployeeDir.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 class EmployeeDir extends Component {
   state = {
-    pictures: [],
+    employees: [],
   }
 
   componentDidMount() {
@@ -11,7 +11,9 @@ class EmployeeDir extends Component {
   }
   loadData = () => {
     axios.get("https://randomuser.me/api/?results=25").then((result) => {
-      console.log(result.data.results[0].picture.medium)
+      console.log(result.data.results)
+      this.setState({ employees:[...result.data.results] })
+
     
     })
   }
@@ -23,23 +25,25 @@ class EmployeeDir extends Component {
         <table className="table">
           <thead>
             <tr>
-              <th scope ="col">#</th>
+              <th scope="col">#</th>
               <th scope="col">Photo</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
+              <th scope="col">Name</th>
               <th scope="col">Email</th>
               <th scope="col">Phone</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Placeholder</td>
-              <td>Doug</td>
-              <td>Jones</td>
-              <td>doug@email.com</td>
-              <td>(206) 123-4567</td>
-            </tr>
+            {this.state.employees.map((employee, index) => {
+              return (
+              <tr key={employee.email}>
+                <th scope="row">{index+1}</th> 
+                <td><img src={employee.picture.medium} alt={employee.name.first} /></td> 
+                <td>{employee.name.first} {employee.name.last}</td>
+                <td>{employee.email}</td>
+                <td>{employee.phone}</td>
+              </tr>
+              )
+            })}
           </tbody>
         </table>
       </>
