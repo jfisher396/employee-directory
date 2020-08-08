13773822e3d810390ffbc10100ec303c8fdd3674
James Fisher <jfisher396@hotmail.com>
2020-08-08 12:21:44 -0700

    deleted unessesary folders

--- src/components/EmployeeDir.js
+++ src/components/EmployeeDir.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import Search from "./Search"
+import Search from "./Search";
 
 class EmployeeDir extends Component {
   state = {
