42cef08d6e9e50bf129b87f50b0dd388f5ac6c69
James Fisher <jfisher396@hotmail.com>
2020-08-08 09:54:07 -0700

    connected to random user api and built basic table to hold data

--- src/components/EmployeeDir.js
+++ src/components/EmployeeDir.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
-export default function EmployeeDir() {
-  return (
-    <div>
-      <h1>Employee table</h1>
-    </div>
-  )
+class EmployeeDir extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+  loadData = () => {
+    axios.get("https://randomuser.me/api/?results=25").then((result) => {
+      console.log(result.data.results[0].name)
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Employee Directory</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Photo</th> 
+              <th scope="col">Name</th>
+              <th scope="col">Last</th> 
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
+
+export default EmployeeDir
