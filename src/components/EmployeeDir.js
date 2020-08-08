ac0882143884ed976f9a5ecdb67db352ebd96eb9
James Fisher <jfisher396@hotmail.com>
2020-08-08 10:58:27 -0700

    mock table data added

--- src/components/EmployeeDir.js
+++ src/components/EmployeeDir.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 class EmployeeDir extends Component {
   state = {
-
+    pictures: [],
   }
 
   componentDidMount() {
@@ -11,7 +11,8 @@ class EmployeeDir extends Component {
   }
   loadData = () => {
     axios.get("https://randomuser.me/api/?results=25").then((result) => {
-      console.log(result.data.results[0].name)
+      console.log(result.data.results[0].picture.medium)
+    
     })
   }
 
@@ -22,15 +23,22 @@ class EmployeeDir extends Component {
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">Photo</th> 
-              <th scope="col">Name</th>
-              <th scope="col">Last</th> 
-              <th scope="col">Handle</th>
+              <th scope ="col">#</th>
+              <th scope="col">Photo</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-
+              <th scope="row">1</th>
+              <td>Placeholder</td>
+              <td>Doug</td>
+              <td>Jones</td>
+              <td>doug@email.com</td>
+              <td>(206) 123-4567</td>
             </tr>
           </tbody>
         </table>
---
+++ src/components/Pictures/Pictures.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EmpPic(props) {
+    return <img src={props.pic} className="EmpPic" alt="Employee" />;
+}
