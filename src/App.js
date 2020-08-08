5ba1e9f413b76b17f59d430e632510f9ee78236e
James Fisher <jfisher396@hotmail.com>
2020-08-08 08:51:06 -0700

    created employee directory file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import EmployeeDir from "./components/EmployeeDir";
+import EmployeeDir from "./components/EmployeeDir";
 
 function App() {
-  return <h1>Employee Directory</h1> ;
+  return <EmployeeDir /> ;
 }
 
 export default App;
